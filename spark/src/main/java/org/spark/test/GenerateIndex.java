c497f086f0e5024e0c5296c325aa0c6099fc5b05
dqanxy <dqanxy6706@gmail.com>
2025-03-18 05:01:20 -0400

    Capability to read from arbitrary S3 files within mapPartitions

---
+++ spark/src/main/java/org/spark/test/GenerateIndex.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.spark.test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.spark.test.Secrets;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.S3Object;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.SparkSession;
+
+public class GenerateIndex {
+
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Usage: JavaWordCount <inputFile> <outputFile>");
+            System.exit(1);
+        }
+
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("SparkJob")
+                .getOrCreate();
+
+        ArrayList test = new ArrayList<String>();
+        test.add("test");
+        test.add("test2");
+
+        JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
+
+        JavaRDD<String> files = sc.parallelize(test);
+
+
+        JavaRDD<String> ans;
+
+        // JavaRDD<String> textFile = spark.read().textFile(args[0])
+        // .filter(
+        //     (org.apache.spark.api.java.function.FilterFunction<String>)
+        //     s -> {
+                
+        //     }).toJavaRDD();
+        ans = files.mapPartitions((org.apache.spark.api.java.function.FlatMapFunction<java.util.Iterator<String>, String>)
+                s -> {
+                    
+                    BasicAWSCredentials awsCreds = new BasicAWSCredentials(Secrets.ACCESS_KEY, Secrets.SECRET_KEY);
+                    AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                            .withRegion("us-east-2") // Set your region
+                            .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+                            .build();
+                    
+                    
+                    List<String> results = new ArrayList<>();
+                    String s3Key = args[0];
+                    
+                    try {
+                        S3Object s3Object = s3Client.getObject("584spark-east2", s3Key);
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(s3Object.getObjectContent()));
+                        String s3Line;
+                        while ((s3Line = reader.readLine()) != null) {
+                            // Process data from S3
+                            results.add("Processed line " + s3Line);
+                        }
+                    } catch (IOException e) {
+                        // Handle exceptions
+                        System.out.println("Error reading file from S3");
+                        e.printStackTrace();
+                    }
+                    return results.iterator();
+                });
+
+        ans.saveAsTextFile(args[1]);
+    }
+}
