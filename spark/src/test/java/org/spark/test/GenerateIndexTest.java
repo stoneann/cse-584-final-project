07a0f5c17715e16fc0a9e1e4ba725efc6469713c
Ann Stone <ann@thestones.me>
2025-03-26 14:55:21 -0400

    Setup testing in junit

---
+++ spark/src/test/java/org/spark/test/GenerateIndexTest.java
@@ -0,0 +1,16 @@
+package org.spark.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class GenerateIndexTest {
+    
+    @Test
+    public void testOne()
+    {
+        System.out.println("hello world");
+        int res = 0;
+        assertEquals(0, res);
+    }
+}
