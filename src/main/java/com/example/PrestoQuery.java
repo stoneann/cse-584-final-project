aa32d062f2359a26f5ea9aeba3f94f20e49e3405
Ann Stone <ann@thestones.me>
2025-04-09 16:46:40 -0400

    working code

--- src/main/java/com/example/PrestoQuery.java
+++ src/main/java/com/example/PrestoQuery.java
@@ -1,22 +1,37 @@
 package com.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Properties;
 
 
 public class PrestoQuery {
     public static void main(String[] args) throws Exception {
-        // final String JDBC_DRIVER = "com.facebook.presto.jdbc.PrestoDriver";
-        
-        // Class.forName(JDBC_DRIVER); 
-        
-        String url = "jdbc:presto://ec2-13-58-144-197.us-east-2.compute.amazonaws.com:8889/hive/default";
-        // String sql = new String(java.nio.file.Files.readAllBytes(java.nio.file.Paths.get("grouped_join.sql")));
-        String sql_query = "SET SESSION grouped_execution = true; SET SESSION recoverable_grouped_execution = true; SELECT t1.join_id, t2.id, t1.id FROM \"10_db\".l_table t1 JOIN \"10_db\".r_table t2 ON t1.join_id = t2.join_id;";
-        try (Connection conn = DriverManager.getConnection(url, "aryanj", null);
-             Statement stmt = conn.createStatement()) {
-            stmt.execute(sql_query);
-            System.out.println("Query executed successfully.");
+        Class.forName("io.prestosql.jdbc.PrestoDriver");
+
+        String url = "jdbc:presto://ip-172-31-36-243.us-east-2.compute.internal:8889/hive/default";
+        Properties props = new Properties();
+        props.setProperty("user", "emrtestuser");
+
+        Connection conn = DriverManager.getConnection(url, props);
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery("SET SESSION grouped_execution = true;\n" + //
+                        "SET SESSION recoverable_grouped_execution = true;\n" + //
+                        "\n" + //
+                        "SELECT t1.join_id, t2.id, t1.id\n" + //
+                        "FROM \"10_db\".l_table t1\n" + //
+                        "JOIN \"10_db\".r_table t2\n" + //
+                        "ON t1.join_id = t2.join_id\n");
+
+        // ResultSet rs = stmt.executeQuery("SELECT * FROM \"10_db\".l_table");
+
+        while (rs.next()) {
+            System.out.println(rs.getString(1));
         }
+
+        rs.close();
+        stmt.close();
+        conn.close();
     }
 }
